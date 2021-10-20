d0c44ce4f0357757c29d1af5c9220703d3ba9570
eren <eozel44@gmail.com>
2021-10-20 12:11:08 +0300

    parse data & initial busines logic

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,42 @@
 import zio.console.putStrLn
 import zio.process.Command
+import zio.stream.ZTransducer
+
 import java.sql.Timestamp
+import zio._
 
 object Main {
 
-  case class data(event_type:String,data:String,timestamp:Timestamp)
+  case class Data(event_type:String,data:String,timestamp:Long)
+  trait ServiceError extends Exception with Product with Serializable
+  case class ParsingError(m: String) extends ServiceError
+
+  def toData(line:String):Data={
+    import io.circe.Decoder
+    import io.circe.generic.semiauto.deriveDecoder
+    import io.circe.parser
+    implicit val decodeData: Decoder[Data] = deriveDecoder[Data]
+    //ZIO.fromEither(parser.decode[Data](line)).mapError(k=>ParsingError(k.getMessage)).
+    parser.decode[Data](line).getOrElse(Data("0","0",0))
+  }
+
+
   val myApp = for {
     _ <- Command("/home/eren/blackbox.amd64").linesStream
-      .foreach(putStrLn(_))
+      //.tap(x => putStrLn(s"before mapping: $x"))
+      .map(k=>toData(k))
+      //.tap(x => putStrLn(s"after mapping: $x"))
+      .chunkN(10)
+      .filterNot(_.timestamp==0)
+      .groupByKey(k=>k.event_type) {
+        case (word, stream) =>
+          stream
+            //.tap(x => putStrLn("aggStreamBefore " + x))
+            .scan((1, word)){ case (acc, _) => (acc._1 + 1, word)}
+            //.tap(x => putStrLn("aggStreamAfter " + x.toString))
+      }
+
+      .foreach(k=>putStrLn(s"${k._2} --- ${k._1}"))
   } yield ()
 
 
